913ae46981792a92b4efedb50c99af7c7657bc3c
Thiago Holder <thiagoholder@gmail.com>
2024-11-06 19:08:03 +0000

    First commit with example project

---
+++ src/Customers.API/Controllers/CustomersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Customers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(new[] { "Customer1", "Customer2", "Customer3" });
+        }
+    }
+}
---
+++ src/Products.API/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Products.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new[] { "Product - 1", "Product - 2", "Product - 3" });
+        }
+    }
+}
